44f1c160e04a8dd90a0129df9af1908c7f9c841c
Mickaël Perrin <dev@mickaelperrin.fr>
2016-07-21 16:30:29 +0200

    [BUGFIX] Unox syncing doesn't work in docker-sync
    - PB: while the command launched has no errors, the sync/watch doesn't
    work.
    - FIX: it looks like that creating a new process instead of a thread
    resolves the issue.
    - TODO: ensure that this is the adequate way to resolve the issue

--- lib/docker-sync/execution.rb
+++ lib/docker-sync/execution.rb
@@ -5,6 +5,22 @@ module Execution
 
   Thread.abort_on_exception = true
 
+  def exec(command, prefix, color)
+    Open3.popen3(command) do |_, stdout, stderr, _|
+
+      # noinspection RubyAssignmentExpressionInConditionalInspection
+      while line_out = stdout.gets
+        say_status prefix, line_out, color
+      end
+
+      # noinspection RubyAssignmentExpressionInConditionalInspection
+      while line_err = stderr.gets
+        say_status prefix, line_err, :red
+      end
+
+    end
+  end
+
   def threadexec(command, prefix = nil, color = nil)
 
     if prefix.nil?
@@ -17,20 +33,25 @@ module Execution
     end
 
     Thread.new {
-      Open3.popen3(command) do |_, stdout, stderr, _|
-
-        # noinspection RubyAssignmentExpressionInConditionalInspection
-        while line_out = stdout.gets
-          say_status prefix, line_out, color
-        end
-
-        # noinspection RubyAssignmentExpressionInConditionalInspection
-        while line_err = stderr.gets
-          say_status prefix, line_err, :red
-        end
-
-      end
+     exec(command, prefix, color)
     }
+
+  end
+
+  def forkexec(command, prefix = nil, color = nil)
+
+    if prefix.nil?
+      # TODO: probably pick the command name without args
+      prefix = 'unknown'
+    end
+
+    if color.nil?
+      color = :cyan
+    end
+
+    Process.fork do
+      exec(command, prefix, color)
+    end
 
   end
 
