8c7596e89a4da24e053a1e1e814b96d723edf474
nbr <nick.braga@bookbub.com>
2016-11-30 17:34:10 -0500

    Preserve forward slash in sync src after expanding path

--- lib/docker-sync/sync_manager.rb
+++ lib/docker-sync/sync_manager.rb
@@ -44,9 +44,14 @@ module Docker_sync
     def upgrade_syncs_config
       @config_syncs.each do |name, config|
         @config_syncs[name]['config_path'] = @config_path
-        # expand the sync source to remove ~ and similar expressions in the path
-        @config_syncs[name]['src'] = File.expand_path(@config_syncs[name]['src'], File.dirname(@config_path))
-        @config_syncs[name]['cli_mode'] = @config_global['cli_mode'] || 'auto'
+
+	# [nbr] convert the sync src from relative to absolute path
+	#	preserve '/' as it may be significant to the sync cmd
+        absolute_path = File.expand_path(@config_syncs[name]['src'])
+	absolute_path << "/" if @config_syncs[name]['src'].end_with?("/")
+	@config_syncs[name]['src'] = absolute_path
+
+	@config_syncs[name]['cli_mode'] = @config_global['cli_mode'] || 'auto'
 
         # set the global verbose setting, if the sync-endpoint does not define a own one
         unless config.key?('verbose')
