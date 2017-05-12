03ae20045a34f5e8be7ab75de1b3f62ca335cf53
Michael Baudino <michael.baudino@alpine-lab.com>
2017-05-12 19:30:11 +0200

    Add missing file :confused:

---
+++ lib/docker-sync/dependencies.rb
@@ -0,0 +1,33 @@
+Dir[
+  File.join(File.dirname(__FILE__), 'dependencies', '**', '*.rb')
+].each { |f| require f }
+
+module DockerSync
+  module Dependencies
+    UNSUPPORTED_OPERATING_SYSTEM = 'Unsupported operating system. Are you sure you need DockerSync?'.freeze
+
+    def self.ensure_all!(config)
+      return ensure_all_for_mac!(config)   if Environment.mac?
+      return ensure_all_for_linux!(config) if Environment.linux?
+      raise(UNSUPPORTED_OPERATING_SYSTEM)
+    end
+
+    def self.ensure_all_for_mac!(config)
+      PackageManager.ensure!
+      Docker.ensure!
+
+      if config.unison_required?
+        Unison.ensure!
+      end
+
+      if config.rsync_required?
+        Rsync.ensure!
+        Fswatch.ensure!
+      end
+    end
+
+    def self.ensure_all_for_linux!(_config)
+      Docker.ensure!
+    end
+  end
+end
