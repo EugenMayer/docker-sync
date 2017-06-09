ae1f95f7e2ffce0134bb44bc606b950a69847950
Michael Baudino <michael.baudino@alpine-lab.com>
2017-06-09 16:41:38 +0200

    Extract an error message to a const

--- spec/helpers/sync_helpers.rb
+++ spec/helpers/sync_helpers.rb
@@ -1,6 +1,8 @@
 RSpec::Matchers.define :be_in_sync_with do |container_dir|
+  CONTAINER_NAME_MISSING = 'Container name is missing. Please chain `.in_container(container_name)` to define it.'.freeze
+
   match do |host_dir|
-    raise "Container name is missing. Please chain `.in_container(container_name)` to define it." if @container_name.nil? || @container_name.empty?
+    raise CONTAINER_NAME_MISSING if @container_name.nil? || @container_name.empty?
     shasums, _stderr, _status = env.execute_inline("cd #{host_dir}; find . -type f -exec shasum -a 256 {} \\;")
     _stdout, _stderr, status  = env.execute_inline("docker exec #{@container_name} bash -c 'cd #{container_dir}; echo -n \"#{shasums}\" | sha256sum -c -'")
     status.success?
