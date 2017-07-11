dfb083b1db1c462e587993f21aa0af0a0ce05fd9
Michael Baudino <michael.baudino@alpine-lab.com>
2017-07-11 10:20:14 +0200

    Remove .rspec and use explicit require 'spec_helper'

--- spec/lib/docker-sync/global_config_spec.rb
+++ spec/lib/docker-sync/global_config_spec.rb
@@ -1,3 +1,4 @@
+require 'spec_helper'
 require 'securerandom'
 require 'tmpdir'
 require 'docker-sync/config/global_config'
--- spec/lib/docker-sync/project_config_spec.rb
+++ spec/lib/docker-sync/project_config_spec.rb
@@ -1,3 +1,4 @@
+require 'spec_helper'
 require 'docker-sync/config/project_config'
 
 RSpec.describe DockerSync::ProjectConfig do
