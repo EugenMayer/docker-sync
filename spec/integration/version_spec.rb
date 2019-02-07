826298ad75efb291a7772fc275756d5b6f33fc52
Ignatius Reza <lyoneil.de.sire@gmail.com>
2019-02-07 10:23:52 +0900

    fix spec

--- spec/integration/version_spec.rb
+++ spec/integration/version_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe '--version', command_execution: :allowed do
   it 'outputs the version' do
     stdout, _stderr, _status = env.execute_inline(subject)
     # puts will always add a newline, so we have to compare against that
-    expect(stdout).to eq UpgradeChecker.get_current_version + "\n"
+    expect(stdout).to eq UpgradeChecker.get_current_version
   end
 
   it 'is successful' do
