9bc0d3300af60fdcf1cb6007fcb5cd8665995072
Simon Kunz <simon@simonkunz.io>
2019-08-21 16:52:54 +0200

    * fix broken options in sync_prefer

--- lib/docker-sync/sync_strategy/unison.rb
+++ lib/docker-sync/sync_strategy/unison.rb
@@ -204,7 +204,7 @@ module DockerSync
         say_status 'ok', "starting initial sync of #{container_name}", :white if @options['verbose']
         # wait until container is started, then sync:
         sync_host_port = get_host_port(get_container_name, UNISON_CONTAINER_PORT)
-        cmd = "unison -testserver #{@options['dest']} \"socket://#{@options['sync_host_ip']}:#{sync_host_port}\""
+        cmd = "unison -testserver #{@options['dest']} \"socket://#{@options['sync_host_ip']}:#{@options['sync_host_port']}\""
         say_status 'command', cmd, :white if @options['verbose']
         attempt = 0
         max_attempt = @options['max_attempt'] || 5
