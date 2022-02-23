2b19fe696afb7a7aa311c8d412f3b24d04e3e547
takahashim <takahashimm@gmail.com>
2022-02-23 16:05:15 +0900

    Modify specs to be more readable

--- spec/lib/docker-sync/docker_compose_session_spec.rb
+++ spec/lib/docker-sync/docker_compose_session_spec.rb
@@ -3,8 +3,6 @@ require 'docker-sync/docker_compose_session'
 require 'docker-sync/command'
 
 RSpec.describe DockerSync::DockerComposeSession do
-  subject(:session) { described_class.new }
-
   let(:exitstatus) { 0 }
   let(:status) { double('exit status', to_s: "pid 12345 exit #{exitstatus}", to_i: exitstatus) }
   let(:output) { 'exit output' }
@@ -23,24 +21,44 @@ RSpec.describe DockerSync::DockerComposeSession do
 
   describe '.new' do
     it 'allows file override' do
-      s1 = described_class.new(files: ['foo.yml'])
+      session = DockerSync::DockerComposeSession.new(files: ['foo.yml'])
       expect(DockerSync::Command).to receive(:run).with('docker-compose', '--file=foo.yml', 'up', dir: nil)
-      s1.up
+      session.up
+    end
+
+    it 'allows more files override' do
+      session = DockerSync::DockerComposeSession.new(files: ['foo.yml', 'bar.yml', 'buz.yml'])
+      expect(DockerSync::Command).to receive(:run).with('docker-compose',
+                                                        '--file=foo.yml',
+                                                        '--file=bar.yml',
+                                                        '--file=buz.yml',
+                                                        'up',
+                                                        dir: nil)
+      session.up
+    end
+
+    it 'allows file and directory override' do
+      session = DockerSync::DockerComposeSession.new(files: ['foo.yml'], dir: './tmp')
+      expect(DockerSync::Command).to receive(:run).with('docker-compose', '--file=foo.yml', 'up', dir: './tmp')
+      session.up
     end
   end
 
   describe '#up' do
     it 'runs containers without build option' do
+      session = DockerSync::DockerComposeSession.new
       expect(DockerSync::Command).to receive(:run).with('docker-compose', 'up', dir: nil)
       session.up
     end
 
     it 'runs containers with build option' do
+      session = DockerSync::DockerComposeSession.new
       expect(DockerSync::Command).to receive(:run).with('docker-compose', 'up', '--build', dir: nil)
       session.up(build: true)
     end
 
     it 'returns captured output' do
+      session = DockerSync::DockerComposeSession.new
       result = session.up
       expect(result).to eq 'exit output'
     end
@@ -48,14 +66,28 @@ RSpec.describe DockerSync::DockerComposeSession do
 
   describe '#down' do
     it 'brings down containers' do
+      session = DockerSync::DockerComposeSession.new
       expect(DockerSync::Command).to receive(:run).with('docker-compose', 'down', dir: nil)
+      session.down
+    end
+
+    it 'brings down containers with files: and dir: options' do
+      session = DockerSync::DockerComposeSession.new(files: ['foo.yml'], dir: './tmp')
+      expect(DockerSync::Command).to receive(:run).with('docker-compose', '--file=foo.yml', 'down', dir: './tmp')
       session.down
     end
   end
 
   describe '#stop' do
     it 'stops containers' do
+      session = DockerSync::DockerComposeSession.new
       expect(DockerSync::Command).to receive(:run).with('docker-compose', 'stop', dir: nil)
+      session.stop
+    end
+
+    it 'stops containers with files: and dir: options' do
+      session = DockerSync::DockerComposeSession.new(files: ['foo.yml'], dir: './tmp')
+      expect(DockerSync::Command).to receive(:run).with('docker-compose', '--file=foo.yml', 'stop', dir: './tmp')
       session.stop
     end
   end
