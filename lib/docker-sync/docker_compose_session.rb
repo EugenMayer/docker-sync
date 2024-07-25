25dffb42efb1b6ceea6e1f42ea1f2cf3637427ae
Alexander Jank <himself@alexanderjank.de>
2024-07-25 15:38:31 +0200

    feat(docker): check for docker-compose binary
    Add compatibility with bother docker-compose v1 and v2 through the docker binary

--- lib/docker-sync/docker_compose_session.rb
+++ lib/docker-sync/docker_compose_session.rb
@@ -26,14 +26,24 @@ module DockerSync
 
     private
 
+    def docker_compose_binary_exists?
+      system('which docker-compose > /dev/null 2>&1')
+    end
+
+
     def run!(*args)
       # file_args and args should be Array of String
       file_args = @files.map { |file| "--file=#{file}" }
 
-      # Prepend "compose" to the args array
-      docker_args = ["compose", *file_args, *args]
+      if docker_compose_binary_exists?
+        command = 'docker-compose'
+        command_args = file_args + args
+      else
+        command = 'docker'
+        command_args = ['compose'] + file_args + args
+      end
 
-      @last_command = Command.run('docker', *docker_args, dir: @dir).join
+      @last_command = Command.run(command, *docker_args, dir: @dir).join
       status = @last_command.status
       out = @last_command.captured_output
       err = @last_command.captured_error
