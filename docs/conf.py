f7bf3da5a2f137bc667d8cead6836b3b4a8a700f
em <eugen.mayer@kontextwork.de>
2024-08-03 08:31:59 +0200

    fix path

--- docs/conf.py
+++ docs/conf.py
@@ -15,8 +15,9 @@ import os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 # import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+import sphinx_rtd_theme
 
 
 # -- Project information -----------------------------------------------------
