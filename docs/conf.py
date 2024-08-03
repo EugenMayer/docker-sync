134ee83dbb4f44f89faf3b60a9bf5de5574517a3
em <eugen.mayer@kontextwork.de>
2024-08-03 08:38:08 +0200

    add dependency

--- docs/conf.py
+++ docs/conf.py
@@ -47,6 +47,8 @@ extensions = [
     "sphinx_rtd_theme",
 ]
 
+html_theme = "sphinx_rtd_theme"
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
